34d316fab56cb1143a895939b7851ea3c1a07fc0
D-Gump <davidbarroetabonilla@gmail.com>
2024-06-29 11:13:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,11 @@ st.write("DataFrame Completo:")
 st.dataframe(car_data)
 
 hist_button = st.button('Construir histograma') # crear un botón
-        
-        if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-            st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
-            fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
-            st.plotly_chart(fig, use_container_width=True)
+
+if hist_button: # al hacer clic en el botón
+         # escribir un mensaje
+        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+         # crear un histograma
+        fig = px.histogram(car_data, x="odometer")
+        # mostrar un gráfico Plotly interactivo
+        st.plotly_chart(fig, use_container_width=True)
